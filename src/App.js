b11b395a850a05814e04b3d0bc5ce6f6158ae193
RenLishisan <renshisan@qq.com>
2020-08-27 20:22:23 +0800

    ä»代码备份

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import {
 const Home = lazy(() => import('./pages/Home'));
 const History = lazy(() => import('./pages/History'));
 const About = lazy(() => import('./pages/About'));
+const Login = lazy(() => import('./pages/Login'));
+const Register = lazy(() => import('./pages/Register'));
 
 
 function App() {
@@ -26,6 +28,8 @@ function App() {
                         <Route path="/" exact component={Home}/>
                         <Route path="/history" component={History}/>
                         <Route path="/about" component={About}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/Register" component={Register}/>
                     </Switch>
                 </Suspense>
             </main>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,14 @@
 import React from "react";
+import {observer} from 'mobx-react'
+import {useStores} from '../stores'
 
-function Component() {
+const Component = observer(()=> {
+    const {AuthStore} = useStores
     return (
         <>
-            <h1>Login</h1>
+            <h1>Login:{AuthStore.values.username}</h1>
         </>
     );
-}
+})
 
 export default Component;
