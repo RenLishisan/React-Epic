038d3ad743429324e1010282d5ff9bfcbcd4c807
RenLishisan <renshisan@qq.com>
2020-08-28 16:04:06 +0800

    添加： 登录、注册 Button 组件样式

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@ import React from "react";
 import LogoUrl from './logo.svg'
 import {NavLink} from "react-router-dom";
 import styled from 'styled-components'
+import {Button} from 'antd';
+import Login from "../pages/Login";
+
 const Header = styled.header`
     background-color:#02101f;
     padding:10px 100px;
@@ -18,17 +21,25 @@ const StyledLink = styled(NavLink)`
         border-bottom:1px solid #FFF
     }
 `
+const StyledButton = styled(Button)`
+    margin-left:10px;
+`
 
 function Components() {
-  return (
-    <Header>
-      <Logo src={LogoUrl}/>
-      <nav>
-          <StyledLink to='/' activeClassName='active' exact>首页</StyledLink>
-          <StyledLink to='/history' activeClassName='active'>上传历史</StyledLink>
-          <StyledLink to='/about' activeClassName='active'>关于我</StyledLink>
-      </nav>
-    </Header>
-  );
+    return (
+        <Header>
+            <Logo src={LogoUrl}/>
+            <nav>
+                <StyledLink to='/' activeClassName='active' exact>首页</StyledLink>
+                <StyledLink to='/history' activeClassName='active'>上传历史</StyledLink>
+                <StyledLink to='/about' activeClassName='active'>关于我</StyledLink>
+            </nav>
+            <nav>
+                <StyledButton type="primary">登录</StyledButton>
+                <StyledButton type="primary">注册</StyledButton>
+            </nav>
+        </Header>
+    );
 }
+
 export default Components;
