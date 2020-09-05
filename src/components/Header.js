847b0a1d907dc3e1fedfadb06b85efd0c04c2a86
RenLishisan <renshisan@qq.com>
2020-09-05 15:28:37 +0800

    修复 用户在登录情况下刷新页面导致登录状态丢失
    Signed-off-by: RenLishisan <renshisan@qq.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,43 @@
-import React, {useState} from "react";
-import LogoUrl from './logo.svg'
-import {NavLink,useHistory} from "react-router-dom";
-import styled from 'styled-components'
-import {Button} from 'antd';
-import {useStores} from '../stores'
-import {observer} from "mobx-react";
+import React, { useState, useEffect } from 'react';
+import LogoUrl from './logo.svg';
+import { NavLink, useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import { Button } from 'antd';
+import { useStores } from '../stores';
+import { observer } from 'mobx-react';
+
 
 const Header = styled.header`
-    background-color:#02101f;
-    padding:10px 100px;
-    display:flex;
-    align-items:center;
-    Color:#FFF
+  display: flex;
+  align-items: center;
+  padding: 10px 100px;
+  background-color: #02101f;
+  color: #fff;
 `;
+
 const Logo = styled.img`
-    height:30px;
-`
+  height: 30px;
+`;
+
 const StyledLink = styled(NavLink)`
-    color:#FFF;
-    margin-left:30px;
-    &.active{
-        border-bottom:1px solid #FFF
-    }
-`
+  color: #fff;
+  margin-left: 30px;
+
+  &.active {
+    border-bottom: 1px solid #fff;
+  }
+`;
+
 const Login = styled.div`
   margin-left: auto;
 `;
-const StyledButton = styled(Button)`
-    margin-left:10px;
-`
 
-const Components = observer(() => {
+const StyledButton = styled(Button)`
+  margin-left: 10px;
+`;
+
+
+const  Component = observer(() => {
 
   const history = useHistory();
   const { UserStore, AuthStore } = useStores();
@@ -38,36 +45,43 @@ const Components = observer(() => {
   const handleLogout = () => {
     AuthStore.logout();
   };
+
   const handleLogin = () => {
     console.log('跳转到登录页面')
-    history.push('/login')
-  }
+    history.push('/login');
+  };
+
   const handleRegister = () => {
-    console.log('跳转到登录页面')
-    history.push('/register')
+    console.log('跳转到注册页面')
+    history.push('/register');
   }
+
+  useEffect(()=>{
+    UserStore.pullUser();
+  },[])
+
   return (
       <Header>
-        <Logo src={LogoUrl}/>
+        <Logo src={LogoUrl} />
         <nav>
-          <StyledLink to='/' activeClassName='active' exact>首页</StyledLink>
-          <StyledLink to='/history' activeClassName='active'>上传历史</StyledLink>
-          <StyledLink to='/about' activeClassName='active'>关于我</StyledLink>
+          <StyledLink to="/" activeClassName="active" exact>首页</StyledLink>
+          <StyledLink to="/history" activeClassName="active">上传历史</StyledLink>
+          <StyledLink to="/about" activeClassName="active">关于我</StyledLink>
         </nav>
         <Login>
           {
-            UserStore.currentUser ? <>以验证ID：
-            {UserStore.currentUser.attributes.username}
-            <StyledButton type="primary" onClick={handleLogout}>注销</StyledButton>
-            </> : <>
+            UserStore.currentUser ? <>已验证ID：
+              {UserStore.currentUser.attributes.username} <StyledButton type="primary" onClick={handleLogout}>注销验证</StyledButton>
+            </> :<>
               <StyledButton type="primary" onClick={handleLogin}>验证通行证</StyledButton>
               <StyledButton type="primary" onClick={handleRegister}>申请通行证</StyledButton>
             </>
-          }
 
+          }
         </Login>
+
       </Header>
   );
-})
+});
 
-export default Components;
+export default Component;
