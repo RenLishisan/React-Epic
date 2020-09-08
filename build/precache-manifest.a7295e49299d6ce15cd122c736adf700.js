f382a83bddfe8ebd21f2dfc16c7e0615a01129c6
RenLishisan <renshisan@qq.com>
2020-09-08 09:39:06 +0800

    init

---
+++ build/precache-manifest.a7295e49299d6ce15cd122c736adf700.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b1ff4500d83871fdc7176a184cccba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90149274e99ceed5ae91",
+    "url": "./static/css/5.847e01e0.chunk.css"
+  },
+  {
+    "revision": "27ffdee9bb0e473c9e54",
+    "url": "./static/css/main.0ce53a76.chunk.css"
+  },
+  {
+    "revision": "61f50035a44cfea2e953",
+    "url": "./static/js/0.63129ec7.chunk.js"
+  },
+  {
+    "revision": "b67c0d665cde5bfe34c3",
+    "url": "./static/js/1.6d4a1e26.chunk.js"
+  },
+  {
+    "revision": "59399e3be9d7c67c488b",
+    "url": "./static/js/10.4ae26819.chunk.js"
+  },
+  {
+    "revision": "e3fc880ead7635dd7ca4",
+    "url": "./static/js/11.c12fd5cb.chunk.js"
+  },
+  {
+    "revision": "fdb0e478efc726068641",
+    "url": "./static/js/12.68f46b92.chunk.js"
+  },
+  {
+    "revision": "54d38826ea2f8e09bca4",
+    "url": "./static/js/2.24fb48ca.chunk.js"
+  },
+  {
+    "revision": "90149274e99ceed5ae91",
+    "url": "./static/js/5.981989cf.chunk.js"
+  },
+  {
+    "revision": "6c48000cb7e3fe060b671159dcdfb038",
+    "url": "./static/js/5.981989cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca834e1779515af7dc90",
+    "url": "./static/js/6.1915933e.chunk.js"
+  },
+  {
+    "revision": "0c28f9fa4011377e0002",
+    "url": "./static/js/7.469a92ac.chunk.js"
+  },
+  {
+    "revision": "da7bc39019090bd14198",
+    "url": "./static/js/8.7c0ce088.chunk.js"
+  },
+  {
+    "revision": "045f2bc6118a9619efa3",
+    "url": "./static/js/9.edf5b006.chunk.js"
+  },
+  {
+    "revision": "27ffdee9bb0e473c9e54",
+    "url": "./static/js/main.334cec50.chunk.js"
+  },
+  {
+    "revision": "63e14c1d050a97b4cc15",
+    "url": "./static/js/runtime-main.aaa4eedd.js"
+  },
+  {
+    "revision": "42906c92f31413e1e332cdcf0941f096",
+    "url": "./static/media/logo.42906c92.svg"
+  }
+]);
--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,9 @@ import React from "react";
 function About() {
   return (
     <>
-      <p>About</p>
+      <p>作者：荏狸十三</p>
+      <p>图姬是一个开源项目，免费提供给大家学习使用，目前属于第一版。</p>
+      <a href="https://github.com/RenLishisan/React-Epic">点击进入项目源代码仓库</a>|<a href="https://www.zhihu.com/people/renlishisan">点击进入作者知乎主页</a>
     </>
   );
 }
